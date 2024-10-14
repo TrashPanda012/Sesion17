17d703f61eeb2c94ed9981d36680a02906f93ee4
TrashPanda012 <kalbrowne@uamv.edu.ni>
2024-10-14 07:21:21 -0600

    Agregué la clase producto

---
+++ Sesion17/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion17.Models
+{
+    public struct Producto
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
